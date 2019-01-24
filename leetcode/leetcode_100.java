fa085ae45055024c7dd1308285541e53bca75db8
chiihero <853879993@qq.com>
2019-01-24 17:06:44 +0800

    up

--- leetcode/leetcode_100.java
+++ leetcode/leetcode_100.java
@@ -35,12 +35,11 @@ public class leetcode_100 {
     }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
-
-
-
-
-        
-return true;
+        if(p==null&&q==null)return true;
+        if(p!=null&&q!=null&&p.val==q.val)
+        return isSameTree(p.left, q.left)&&isSameTree(p.right, q.right);
+        else
+        return false;
     }
 
     public static void main(String[] args) {
