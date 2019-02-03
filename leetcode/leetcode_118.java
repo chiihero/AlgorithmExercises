9af0fa34aee64c1ac82d89c1f8c31fc7d8a30d24
chiihero <853879993@qq.com>
2019-02-03 12:22:37 +0800

    up

---
+++ leetcode/leetcode_118.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+118. 杨辉三角
+给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+
+
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 5
+输出:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+
+public class leetcode_118 {
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+
+        if (numRows==0) {
+            return triangle;
+        }
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+        for(int i=1;i<numRows;i++){
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(i-1);
+            row.add(1);
+            for(int j=1;j<i;j++){
+                row.add(prevRow.get(j)+prevRow.get(j-1));
+            }
+            row.add(1);
+            triangle.add(row);
+        }
+        return triangle;
+    }
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+    
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+    
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+    
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        List<List<Integer>> ret = generate(5);
+            
+        String out = int2dListToString(ret);
+        
+        System.out.print(out);  
+    }
+}
