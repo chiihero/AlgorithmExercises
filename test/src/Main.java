93e6d6aac7e572de40daf2d75d633aad4347a977
chiihero <853879993@qq.com>
2019-03-22 23:07:42 +0800

    up

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,28 +1,34 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Scanner;
 import java.util.Stack;
+import java.util.Vector;
 
 public class Main {
+    public static void f(int[] a, int k, int n, String s)
+    {
+        if(k==a.length){
+            if(n==0) System.out.println(s);
+            return;
+        }
 
+        String s2 = s;
+        for(int i=0; i<=a[k]; i++){
+            f(a, k+1, 5-s2.length(), s2);   //填空位置
+            s2 += (char)(k+'A');
+        }
+    }
+    public static boolean check(){
+    Math.pow(9, 1/3);
+
+    }
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();    //获取开始时间
-        int n=50;
-        boolean[] is_composite = new boolean[n + 1];
-        int count = n / 2;
-        int stop_at= (int) Math.sqrt(n)+1;
+        int[] a = {4,2,2,1,1,3};
 
-        for (int i = 3; i <stop_at; i += 2) {
-            if (is_composite[i])
-                continue;
-            for (int j = i * i; j < n; j += 2 * i) {
-                if (!is_composite[j]) {
-                    count--;
-                    is_composite[j] = true;
-                }
-            }
-        }
-        System.out.println(count);
+        f(a,0,5,"");
         long endTime = System.currentTimeMillis();    //获取结束时间
         System.out.println("程序运行时间：" + (endTime - startTime) + "ms");    //输出程序运行时间
     }
+
 }
